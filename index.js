988b458c6dc88fa960c24aefc75bf775275300fb
himanoa <matsunoappy@gmail.com>
2017-01-03 20:46:22 +0900

    implement formulaToRpn

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ var formulaToRpn = function(formula) {
    * @type {string}
    **/
 
+  var rpnRank = {
+    '(': 4,
+    '#': 3,
+    '*': 2,
+    '/': 2,
+    '+': 1,
+    '-': 1,
+    ')': 0
+  };
   formula = formula.split("");
   var stack = new Array();
   formula.unshift('(');
@@ -26,7 +35,34 @@ var formulaToRpn = function(formula) {
   var tmp = "";
 
   formula.forEach(function(val){
+    if(val.match(/[0-9]/)){
+      tmp += val;
+    } else {
+      if(val != "") {
+        if(result.length > 0) {
+          result += ' ';
+        }
+        result += tmp;
+        tmp = '';
+      }
 
-  })
+      while (stack && rpnRank[stack[stack.length-1]] >= rpnRank[val] && stack[stack.length-1] != '(') {
+        if(result.length > 0){
+          result += ' ';
+        }
+        result += stack.pop();
+      }
+      if (val == ')') {
+        stack.pop();
+      } else {
+        stack.push(val);
+      }
+    }
+  });
+  return result;
+}
 
+module.exports = {
+  calcFormula: calcFormula,
+  formulaToRpn: formulaToRpn
 }
